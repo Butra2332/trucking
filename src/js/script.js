03ef0e352f4ea3f61cfba961a1ccfc8911889fcf
Antoxa <bytrik2332@tut.by>
2021-03-25 15:37:54 +0300

    add section review

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,11 @@
 "use strict";
+$(document).ready(function(){
+    $('.review-carousel').slick({
+        speed: 300,
+        slidesToShow: 1,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg""></button>'
+    });
+});
 
 
